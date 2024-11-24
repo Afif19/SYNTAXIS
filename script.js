f6e595644ac3f315e6b30196cb3cb0cd7dcea30b
Afif19 <afifuddinmahfud7@gmail.com>
2024-11-24 20:36:59 +0700

    by afif3

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const hospitals = [
+    { id: 1, name: 'RSUD Sleman ' },
+    { id: 2, name: 'RS Puri Husada' },
+    { id: 3, name: 'RS JIH' },
+];
 
+const doctors = [
+    { id: 1, name: 'dr. Ahmad', speciality: 'Poli Umum', schedule: '08:00 - 12:00', image: 'doctor1.jpg' },
+    { id: 2, name: 'dr. Sarah', speciality: 'Poli Anak', schedule: '13:00 - 17:00', image: 'doctor2.jpg' },
+    { id: 3, name: 'dr. Michael', speciality: 'Poli Gigi', schedule: '09:00 - 15:00', image: 'doctor3.jpg' },
+];
